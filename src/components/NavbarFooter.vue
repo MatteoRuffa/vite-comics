0599bc72edde0134b12dfb34aceba75d699830f7
MatteoRuffa <cortese_17@protonmail.com>
2024-04-10 15:31:19 +0200

    add first drafr navbarFooter

--- src/components/NavbarFooter.vue
+++ src/components/NavbarFooter.vue
@@ -1,7 +1,14 @@
 <template>
     <section>
         <div class="container">
-            prova
+            <ul>
+                <li v-for="item in navbarFooter" :key="item.name">
+                    <a :href="item.link">
+                        <img :src="item.img" :alt="item.name">
+                        <span>{{ item.name }}</span>
+                    </a>
+                </li>
+            </ul>
         </div>
     </section>
 </template>
@@ -9,6 +16,17 @@
 <script>
     export default {
         name: 'NavbarFooter',
+        data () {
+            return{
+                navbarFooter: [
+                    {name: 'digital comics', link: '#', img: '/img/buy-comics-digital-comics.png'},
+                    {name: 'dc merchandise', link: '#', img: '/img/buy-comics-merchandise.png'},
+                    {name: 'subscription', link: '#', img: '/img/buy-comics-subscriptions.png'},
+                    {name: 'comic shop locator', link: '#', img: '/img/buy-comics-shop-locator.png'},
+                    {name: 'dc power visa', link: '#', img: '/img/buy-dc-power-visa.svg'}
+                ]
+            }
+        }
     }
 </script>
 

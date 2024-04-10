f0a933c8f49cee78c0f84239135115bbc1f16ca7
MatteoRuffa <cortese_17@protonmail.com>
2024-04-10 16:50:35 +0200

    add draft sectionbottomfooter

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,17 +2,20 @@
     <footer>
         <NavbarFooter />
         <SectionLinkFooter />
+        <SectionBottomFooter />
     </footer>
 </template>
 
 <script>
 import NavbarFooter from './NavbarFooter.vue';
 import SectionLinkFooter from './SectionLinkFooter.vue';
+import SectionBottomFooter from './SectionBottomFooter.vue';
     export default {
         name: 'FooterComponent',
         components: {
             NavbarFooter,
             SectionLinkFooter,
+            SectionBottomFooter
         }
     }
 </script>
---
+++ src/components/SectionBottomFooter.vue
@@ -0,0 +1,57 @@
+<template>
+    <section>
+        <div class="container">
+            <div>
+                <a href="#">sign-up now!</a>
+            </div>
+            <div>
+                <span>follow us</span>
+                <img src="/img/footer-facebook.png" alt="">
+                <img src="/img/footer-twitter.png" alt="">
+                <img src="/img/footer-youtube.png" alt="">
+                <img src="/img/footer-pinterest.png" alt="">
+                <img src="/img/footer-periscope.png" alt="">
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'SectionBottomFooter',
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/style/partials/variables' as *;
+    section {
+        background-color: $bg-bottom-footer;
+        padding: 30px 0;
+        .container {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            gap: 20px;
+
+            a {
+            text-decoration: none;
+            text-transform: uppercase;
+            color: $light;
+            border: 2px solid $text-hover;
+            padding: 10px 20px;
+            }
+
+            span {
+                color: $text-hover;
+                text-transform: uppercase;
+                font-weight: 700;
+                padding: 0 20px;
+            }
+            img {
+                padding: 0 10px;
+            }
+        }
+        
+    }
+
+</style>
